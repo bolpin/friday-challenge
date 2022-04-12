4097ca688a40764eed0275f89cbe503cc88500ef
Brian Olpin <bolpin@gmail.com>
2022-04-12 09:03:25 -0700

    moving http requests to store for players actions

--- src/store/players-actions.js
+++ src/store/players-actions.js
@@ -1,16 +1,123 @@
-import { uiActions } from './ui-slice';
-import { playersActions } from './players-slice';
-import { apiRoot } from '../config';
+import { uiActions } from "./ui-slice";
+import { playersActions } from "./players-slice";
+import { apiRoot } from "../config";
+
+export const deletePlayer = (playerId) => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(`${apiRoot}/players/${playerId}.json`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Could not delete player");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const playersData = await fetchData();
+      dispatch(
+        playersActions.removePlayer(
+          playersId
+        )
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Delete player failed",
+        })
+      );
+    }
+  };
+};
+
+export const updatePlayer = (player) => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(`${apiRoot}/players.json`, {
+        url: `${apiRoot}/players/${player.id}.json`,
+        method: "PATCH",
+        body: {
+          player: player,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Could not update player");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const playersData = await fetchData();
+      dispatch(playersActions.updatePlayer(player));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Update player failed",
+        })
+      );
+    }
+  };
+};
+
+export const createPlayer = (player) => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(`${apiRoot}/players.json`, {
+        method: "POST",
+        body: {
+          player: player,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Could not create player");
+      }
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const playersData = await fetchData();
+      dispatch(playersActions.addPlayer(player));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Update player failed",
+        })
+      );
+    }
+  };
+};
 
 export const fetchPlayers = () => {
   return async (dispatch) => {
     const fetchData = async () => {
-      const response = await fetch(
-        `${apiRoot}/players.json`
-      );
+      const response = await fetch(`${apiRoot}/players.json`);
 
       if (!response.ok) {
-        throw new Error('Could not fetch players!');
+        throw new Error("Could not fetch players");
       }
 
       const data = await response.json();
@@ -28,9 +135,9 @@ export const fetchPlayers = () => {
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Fetching players failed!',
+          status: "error",
+          title: "Error",
+          message: "Fetching players failed!",
         })
       );
     }
--- src/store/players-slice.js
+++ src/store/players-slice.js
@@ -11,8 +11,8 @@ const playersSlice = createSlice({
       state.players = action.payload.players;
     },
     addPlayer(state, action) {
-      const newPlayer = action.payload;
       state.changed = true;
+      const newPlayer = action.payload.newPlayer;
       state.players.push({
         id: newPlayer.id,
         first_name: newPlayer.firstName,
@@ -21,10 +21,19 @@ const playersSlice = createSlice({
         gender_id: newPlayer.genderId,
       });
     },
+    updatePlayer(state, action) {
+      state.changed = true;
+      const playerId = action.payload.id;
+      const existingPlayer = state.players.find((p) => p.id === id);
+      existingPlayer.gender_id = action.payload.genderId
+      existingPlayer.first_name = action.payload.firstName
+      existingPlayer.last_name = action.payload.firstName
+      existingPlayer.birthdate = action.payload.firstName
+    },
     removePlayer(state, action) {
+      state.changed = true;
       const id = action.payload;
       const existingPlayer = state.players.find((p) => p.id === id);
-      state.changed = true;
       state.players = state.players.filter((p) => p.id !== id);
     },
   },
