4dcc866704c351d95dc0cd2fdecbebe268c627fa
Brian Olpin <bolpin@gmail.com>
2022-04-13 15:55:31 -0700

    cleaning up unused files

--- src/config.js
+++ src/config.js
@@ -1,55 +1,5 @@
 const apiRoot = 'http://localhost:3000';
 
-// for an example of a dynamic lookup table over
-// http, see /stores/players-*.js
-const locales = [
-  {
-    id: 1,
-    code: "en_US",
-  },
-  {
-    id: 2,
-    code: "es_AR",
-  },
-  {
-    id: 3,
-    code: "es_MX",
-  },
-  {
-    id: 4,
-    code: "zh_TW",
-  },
-];
-
-const genders = [
-  {
-    id: 1,
-    name: "female",
-  },
-  {
-    id: 2,
-    name: "male",
-  },
-  {
-    id: 3,
-    name: "non-binary",
-  },
-]; 
-
-const operatingSystems = [
-  {
-    id: 1,
-    name: "android",
-  },
-  {
-    id: 2,
-    name: "ios",
-  }
-];
-
 export {
   apiRoot,
-  locales,
-  genders,
-  operatingSystems
 };
